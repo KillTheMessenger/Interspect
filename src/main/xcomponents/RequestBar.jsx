3b67212491816fc1ff0c2524c513452ead5e1e7f
Natia Khomasuridze <natiakhoma@gmail.com>
2019-09-18 21:24:19 -0400

    hit the block

--- src/main/xcomponents/RequestBar.jsx
+++ src/main/xcomponents/RequestBar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import Button from './styledComponents/Button';
 import Form from './styledComponents/Form';
--- src/main/xcontainers/XMockupsPanel.js
+++ src/main/xcontainers/XMockupsPanel.js
@@ -1,8 +1,12 @@
+/* eslint-disable */
+
 import React, { Component } from 'react';
 // import RequestBar from '../components/RequestBar.jsx';
 import PropTypes from 'prop-types';
 import StyledPanel from './StyledPanel.jsx';
 // import PerformanceMetrics from '../components/PerformanceMetrics.jsx';
+
+import Main from '../xcomponents/Main.jsx';
 
 export default class MockupsPanel extends Component {
   render() {
@@ -15,6 +19,7 @@ export default class MockupsPanel extends Component {
         style={{ cursor: 'pointer' }}
         >
         <h1>Mockups</h1>
+        {/* <Main/> */}
       </StyledPanel>
     )
   }
